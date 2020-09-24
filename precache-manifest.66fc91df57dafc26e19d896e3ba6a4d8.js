074b760e262ff739426db8d1b8fa57564046e59f
sidhant-ships <sidhant.learn@gmail.com>
2020-09-24 06:40:11 +0530

    Updates

---
+++ precache-manifest.66fc91df57dafc26e19d896e3ba6a4d8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d040a38e02052a631ef3a02b623017f1",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "1cf071891b6702b580b6",
+    "url": "/me/static/css/main.5b003403.chunk.css"
+  },
+  {
+    "revision": "f127cc69deea19dbb1f9",
+    "url": "/me/static/js/2.b1b459ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.b1b459ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf071891b6702b580b6",
+    "url": "/me/static/js/main.e5f973a6.chunk.js"
+  },
+  {
+    "revision": "43e1106f74f1c139955e",
+    "url": "/me/static/js/runtime-main.8e474a00.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/me/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/me/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/me/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/me/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/me/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/me/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/me/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/me/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/me/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/me/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/me/static/media/skill.34409398.svg"
+  }
+]);
